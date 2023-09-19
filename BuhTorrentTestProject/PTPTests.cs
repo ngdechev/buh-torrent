f71746f518a7759e4c265c757eb7d5bf788726fb
STANILGP <47471510+STANILGP@users.noreply.github.com>
2023-09-19 15:17:07 +0300

    PTPTests(noWork)
    In PTP.cs Is needed a Constuctor().

---
+++ BuhTorrentTestProject/PTPTests.cs
@@ -0,0 +1,39 @@
+﻿using ProtocolParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuhTorrentTestProject
+{
+    [TestFixture]
+    public class PTPTests
+    {
+        [Test]
+        public void ToPackage_ReturnsCorrectString()
+        {
+            //// Arrange
+            //PTP_Block block = new PTP_Block(1, 5, "hello");
+
+            //// Act
+            //string result = block.ToPackage();
+
+            //// Assert
+            //Assert.AreEqual("105hello", result);
+        }
+
+        [Test]
+        public void IsType_ReturnsTrueForAvailable()
+        {
+            //// Arrange
+            //PTP_Block block = new PTP_Block(0, 10, "available");
+
+            //// Act
+            //bool result = block.IsType("available");
+
+            //// Assert
+            //Assert.IsTrue(result);
+        }
+    }
+}
