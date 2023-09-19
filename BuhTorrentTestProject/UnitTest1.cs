9d1eb6601d480cfce41f328c5fa0f46f3d4715a1
ngdechev <n.dechev@abv.bg>
2023-09-19 13:26:12 +0300

    Add project files.

---
+++ BuhTorrentTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BuhTorrentTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
