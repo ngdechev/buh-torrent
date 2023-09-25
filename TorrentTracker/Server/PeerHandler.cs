e81cfab972a7c83383af685f30115e834688ed97
ngdechev <n.dechev@abv.bg>
2023-09-25 13:15:47 +0300

    Implemented TrackerServer and added empty entity classes.

---
+++ TorrentTracker/Server/PeerHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Sockets;
+
+namespace TorrentTracker.Server
+{
+    public class PeerHandler
+    {
+        private TcpClient _peerSocket;
+        private NetworkStream _stream;
+        private TrackerServer _server;
+
+        public PeerHandler(TcpClient peerSocket, TrackerServer server)
+        {
+            _peerSocket = peerSocket ?? throw new ArgumentNullException(nameof(peerSocket));
+            _server = server ?? throw new ArgumentNullException(nameof(server));
+        }
+
+        public void HandlePeer()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SendMessage(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
