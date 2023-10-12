acd21b579cd2eaee9769d9ba7e9d10cc88d613e1
Yanislav <114386411+yanislav2002@users.noreply.github.com>
2023-10-12 12:23:54 +0300

    Updated PeerHandler and TrackerServer

--- TorrentTracker/Server/PeerHandler.cs
+++ TorrentTracker/Server/PeerHandler.cs
@@ -8,17 +8,12 @@ namespace TorrentTracker.Server
     {
         private TcpClient _peerSocket;
         private NetworkStream _stream;
-        private TrackerServer _server;
-        //private PTT _peerToTracker = new PTT();
         private ITorrentManagementController _torrentManagementController;
         private IPeerManagementController _peerManagementController;
         
-        private bool _isRunning = false;
-
-        public PeerHandler(TcpClient peerSocket, TrackerServer server, ITorrentManagementController torrentManagementController, IPeerManagementController peerManagementController)
+        public PeerHandler(TcpClient peerSocket, ITorrentManagementController torrentManagementController, IPeerManagementController peerManagementController)
         {
             _peerSocket = peerSocket; 
-            _server = server;
             _torrentManagementController = torrentManagementController;
             _peerManagementController = peerManagementController;
         }
@@ -29,58 +24,50 @@ namespace TorrentTracker.Server
             
             _stream = _peerSocket.GetStream();
 
-            _isRunning = true;
+            _block = PTT.ParseToBlock(_stream);
 
-            while (_isRunning)
+            byte command = _block.GetCommand();
+            int size = _block.GetSize();
+            string payload = _block.GetPayload();
+
+            if (command == 0x00)
             {
-                _block = PTT.ParseToBlock(_stream); //_peerToTracker.ParseToBlock(_stream);
-                string command = _block.GetCommand();
-                string payload = _block.GetPayload();
-
-                if (command == "0x00")
-                {
-                    _peerManagementController.CreatePeer(payload);
-                } 
-                else if (command == "0x01")
-                {
-                    _peerManagementController.DestroyPeer(payload);
-                }
-                else if (command == "0x02")
-                {
-                    string[] payloadArray = payload.Split(";",2);
-
-                    string ip = payloadArray[0];
-                    string torrentFile = payloadArray[1];
-
-                    _torrentManagementController.CreateTorrent(ip, torrentFile);
-                }
-                else if (command == "0x03")
-                {
-                    string[] payloadArray = payload.Split(" ");
-
-                    string ip = payloadArray[0];
-                    string checksum = payloadArray[1];
-
-                    _torrentManagementController.DeleteTorrent(ip, checksum);
-                }
-                else if (command == "0x04")
-                {
-                    _torrentManagementController.ListTorrents();
-                }
-                else if (command == "0x06")
-                {
-                    _peerManagementController.ListPeers();
-                }
-                else if (command == "0x08")
-                {
-                    _torrentManagementController.SearchTorrent(payload);
-                }
+                _peerManagementController.CreatePeer(payload);
+            } 
+            else if (command == 0x01)
+            {
+                _peerManagementController.DestroyPeer(payload);
             }
-        }
+            else if (command == 0x02)
+            {
+                string[] payloadArray = payload.Split(";",2);
 
-        public void Disconnect()
-        {
-            _isRunning = false;
+                string ip = payloadArray[0];
+                string torrentFile = payloadArray[1];
+
+                _torrentManagementController.CreateTorrent(ip, torrentFile);
+            }
+            else if (command == 0x03)
+            {
+                string[] payloadArray = payload.Split(" ");
+
+                string ip = payloadArray[0];
+                string checksum = payloadArray[1];
+
+                _torrentManagementController.DeleteTorrent(ip, checksum);
+            }
+            else if (command == 0x04)
+            {
+                _torrentManagementController.ListTorrents();
+            }
+            else if (command == 0x06)
+            {
+                _peerManagementController.ListPeers();
+            }
+            else if (command == 0x08)
+            {
+                _torrentManagementController.SearchTorrent(payload);
+            }
         }
     }
 }
