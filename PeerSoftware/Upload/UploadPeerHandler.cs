6ce748a3321833b5978390e1722e43f345cc463c
zlati <zlatin.velikov@gmail.com>
2023-10-31 16:37:41 +0200

    done

---
+++ PeerSoftware/Upload/UploadPeerHandler.cs
@@ -0,0 +1,105 @@
+﻿using PeerSoftware.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using PTP_Parser;
+
+namespace PeerSoftware.Upload
+{
+    internal class UploadPeerHandler
+    {
+        ITorrentStorage _storage;
+        TcpClient _tcpClient;
+        bool _isRunning;
+        List<PTPBlock> _blocks;
+        public UploadPeerHandler(ITorrentStorage storage, TcpClient client) 
+        {
+            _isRunning = true;
+            _storage = storage;
+            _tcpClient = client;
+        }
+
+
+        public void Handle()
+        {
+            while (_isRunning)
+            {
+                NetworkStream stream = _tcpClient.GetStream();
+                PTPBlock block = PTPParser.ParseToBlock(stream);
+                if (block.IsType("StartPackage"))
+                {
+                    string[] data = block.GetData().Split('/',3);
+                    Disasemble(data[1], data[2]);
+                    foreach(PTPBlock pTPBlock in _blocks)
+                    {
+                        stream.Write(PTPParser.ParseToPackage(pTPBlock));
+                    }
+                    stream.Write(PTPParser.UnavailablePackage());
+                }
+            }
+        }
+
+        public void Disasemble(string cheksum, string blocks)
+        {
+            TorrentFile torrentFile = _storage.GetMyTorrentFiles().Find(r => r.info.checksum == cheksum);
+            if (torrentFile != null)
+            {
+                _blocks = new List<PTPBlock>();
+                string filePath = torrentFile.info.fileName; // Replace with the path to your file
+                string[] idBlocks = blocks.Split('-', 2);
+                int.TryParse(idBlocks[0],out int firstBlock );
+                int.TryParse(idBlocks[1],out int lastBlock );
+                int startPosition = (firstBlock-1) * 1016 ; // Start position in the file
+                int lengthToRead = 0;
+                bool isFull = false;
+                if (((lastBlock - firstBlock) * 1016 + startPosition) <= torrentFile.info.length)
+                {
+                     lengthToRead = (lastBlock - firstBlock) * 1016; // Number of bytes to read
+                     isFull= true;
+                }
+                else
+                {
+                    lengthToRead = (lastBlock - 1 - firstBlock) * 1016;
+                    lengthToRead = lengthToRead + (int)(torrentFile.info.length - ((lastBlock-1)*1016));
+                    isFull = false;
+                }
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    fs.Seek(startPosition, SeekOrigin.Begin);
+
+                    using (BinaryReader br = new BinaryReader(fs))
+                    {
+                        byte[] buffer = br.ReadBytes(lengthToRead);
+                        _blocks.Clear();
+                        if (isFull)
+                        {
+                            for (int i = 1; i <= (lastBlock - firstBlock) ; i++)
+                            {
+                                byte[] bytes = new byte[1016];
+                                Array.Copy(buffer, ((i-1)*1016) , bytes, 0 , 1016);
+                                _blocks.Add(new PTPBlock(i, bytes.Length, bytes));
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 1; i <= (lastBlock - firstBlock - 1); i++)
+                            {
+                                byte[] bytes = new byte[1016];
+                                Array.Copy(buffer, ((i - 1) * 1016), bytes, 0, 1016);
+                                _blocks.Add(new PTPBlock(i, bytes.Length, bytes));
+                            }
+                            int size = buffer.Length - ((lastBlock- firstBlock- 1)*1016);
+                            byte[] lastBytes = new byte[size];
+                            Array.Copy(buffer, ((lastBlock - firstBlock - 1) * 1016), lastBytes, 0, size);
+                            _blocks.Add(new PTPBlock(lastBlock, lastBytes.Length, lastBytes));
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
